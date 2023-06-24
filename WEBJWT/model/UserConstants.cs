5fdd1511adb07c3e2a63d092d7185582c6125569
jobin4jo <jobins802@gmail.com>
2023-06-24 20:43:34 +0530

    second

---
+++ WEBJWT/model/UserConstants.cs
@@ -0,0 +1,25 @@
+﻿namespace WEBJWT.model
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "jason_admin", EmailAddress = "jason.admin@email.com", Password = "MyPass_w0rd", GivenName = "Jason", Surname = "Bryant", Role = "Administrator" },
+            new UserModel() { Username = "elyse_seller", EmailAddress = "elyse.seller@email.com", Password = "MyPass_w0rd", GivenName = "Elyse", Surname = "Lambert", Role = "Seller" },
+        };
+    }
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public string Role { get; set; }
+        public string Surname { get; set; }
+        public string GivenName { get; set; }
+    }
+    public class UserLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
